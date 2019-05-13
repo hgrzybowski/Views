fcdb789a54f8537a97b25858c0927f04e9f92f4a
hgrzybowski <h-grzybowski@wp.pl>
2019-05-13 14:12:19 +0200

    Addchange

--- View/Global.asax.cs
+++ View/Global.asax.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using View.App_Start;
 
 namespace View
 {
@@ -13,6 +11,7 @@ namespace View
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
